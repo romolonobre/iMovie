18a554d25cf6712436b3ca07094835a2a1a8cda0
romolonobre <romoloferreira@icloud.com>
2024-07-23 14:58:47 +0100

    new splashscreen

--- lib/app/spash_screen.dart
+++ lib/app/spash_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:imovie_app/app/authentication/data/firebase_auth_service.dart';
+import 'package:imovie_app/app/commons/app_services/utils.dart';
 import 'package:imovie_app/app/commons/imovie_ui/iui_text.dart';
-import 'package:imovie_app/app/commons/remote_config/remote_config.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -18,6 +19,8 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     final user = service.getCurrentUser();
+    //
+    // Check if user is logged or not, if user is null it means the user is logged out
     final String navigatoPath = user != null ? "home" : "login";
     Future.delayed(const Duration(seconds: 2)).then((value) => Modular.to.navigate('/$navigatoPath/'));
   }
@@ -25,39 +28,24 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: backgroundColor,
       body: Stack(
         children: [
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/images/splash_background.jpeg"),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
           Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 IUIText.title(
                   "IMovie",
-                  color: Colors.white,
-                  fontsize: 35,
-                ),
-                IUIText.heading(
-                  "Find your favorite movies",
-                  color: Colors.white,
-                  fontsize: 20,
-                ),
+                  color: primaryColor,
+                  fontsize: 50,
+                  fontWeight: FontWeight.w800,
+                ).animate().animate().fade().scale(delay: 500.ms)
               ],
             ),
           )
         ],
       ),
     );
-  }
-
-  String getInitialNavigationPath() {
-    return CustomRemoteConfig().getValueOrDefault(key: "initialNavigationPath", defaultValue: "home");
   }
 }
