dc2b782e98e4bc4933be4f71ca5faedade38af58
romolonobre <romoloferreira@icloud.com>
2024-07-31 21:10:15 +0100

    remove unsed code

--- lib/app/commons/app_services/helper.dart
+++ lib/app/commons/app_services/helper.dart
@@ -50,35 +50,6 @@ class Helper {
     return 0.0;
   }
 
-  /// Converts [value] to a double, returns 0.0 on failure or unconverteble value.
-  static double? getDoubleOrNull(dynamic value) {
-    if (value == null) {
-      return null;
-    }
-
-    if (value is double) {
-      return value;
-    }
-
-    /// check for [int] types
-    if (value is int) {
-      return value.toDouble();
-    }
-
-    /// check for [String] types
-    if (value is String) {
-      if (value.isNotEmpty) {
-        try {
-          return double.parse(value);
-        } catch (error) {
-          // do nothing, this value is not parse-able.
-        }
-      }
-    }
-
-    return null;
-  }
-
   /// Converts [value] to an int, returns 0 on failure or unconverteble value.
   static int getInt(dynamic value) {
     if (value == null) {
@@ -107,35 +78,6 @@ class Helper {
     }
 
     return 0;
-  }
-
-  /// Converts [value] to an int, returns 0 on failure or unconverteble value.
-  static int? getIntOrNull(dynamic value) {
-    if (value == null) {
-      return null;
-    }
-
-    if (value is int) {
-      return value;
-    }
-
-    if (value is double) {
-      return value.toInt();
-    }
-
-    /// check for [String] types
-    if (value is String) {
-      if (value.isNotEmpty) {
-        try {
-          return int.parse(value);
-        } catch (error) {
-          // re assume this means the value is actually a double, as a string.
-          return getIntOrNull(double.parse(value));
-        }
-      }
-    }
-
-    return null;
   }
 
   /// Parses the [value] as a string. If the value is a Map or List, it will
@@ -192,63 +134,6 @@ class Helper {
     }
 
     return null;
-  }
-
-  /// Parses the [value] as a list of string values. If [lowerCase] is true,
-  /// the values with be lower cased.
-  static List<String> getStringList(dynamic value, {bool lowerCase = false}) {
-    if (value == null) {
-      return <String>[];
-    }
-
-    /// check for [Map] type
-    if (value is Map || value is List) {
-      if (value.length == 0) {
-        return <String>[];
-      }
-
-      final List<dynamic> listValue = value as List<dynamic>;
-
-      final List<String> items = [];
-      for (final dynamic str in listValue) {
-        String newStr = Helper.getString(str);
-
-        if (lowerCase == true) {
-          newStr = newStr.toLowerCase();
-        }
-
-        items.add(newStr);
-      }
-
-      return items;
-    }
-
-    /// check for [String] types
-    if (value is String) {
-      String str = Helper.getString(value);
-
-      if (lowerCase == true) {
-        str = value.toLowerCase();
-      }
-
-      return <String>[str];
-    }
-
-    /// check for [int] and [double] types
-    if (value is int || value is double) {
-      return <String>[value.toString()];
-    }
-
-    return <String>[];
-  }
-
-  // Make sure we return a map if the input is a list
-  static Map<String, dynamic> getMap(dynamic value) {
-    if (value is! Map) {
-      return {};
-    }
-
-    return value as Map<String, dynamic>;
   }
 }
 
