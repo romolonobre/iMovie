d3f392214455eb3ff245a16228e64fa5f7ab3967
romolonobre <romoloferreira@icloud.com>
2024-08-08 20:27:19 +0100

    create CustomSearchController

---
+++ lib/app/_commons/flutter_widgets/search/search_controller.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'search_service.dart';
+import 'search_state.dart';
+
+class CustomSearchController extends ValueNotifier<SearchState> {
+  CustomSearchController() : super(SearchIdleState());
+
+  void _emit(SearchState state) => value = state;
+
+  final service = SearchService();
+  Future<void> search(String value) async {
+    _emit(SearchLoadingState());
+    final result = await service.search(value);
+    _emit(result);
+  }
+}
